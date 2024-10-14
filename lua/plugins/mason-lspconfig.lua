9b93db4a78a0938f39a881c349574a16ddc57d64
Santiago Soler <santisoler@fastmail.com>
2024-10-14 14:19:52 -0700

    Add nvim-cmp to mason-lspconfig.lua

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -5,18 +5,120 @@ function config_mason_lsp()
         ensure_installed = { "lua_ls", "pylsp" },
     }
 
+    -- Configure nvim-cmp
+    config_cmp()
+
     -- Setup servers
-    require("lspconfig").lua_ls.setup{}
-    require("lspconfig").pylsp.setup{}
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    require("lspconfig").lua_ls.setup{
+      capabilities = capabilities
+    }
+    require("lspconfig").pylsp.setup{
+      capabilities = capabilities
+    }
 end
 
+function config_cmp()
+  -- Set up nvim-cmp.
+  local cmp = require'cmp'
+
+  -- Define select_opts (used for Tab and Shift+Tab)
+  local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+      -- Use Tab and Shift+Tab to explore autocomplete options
+      -- (source: https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/)
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        local col = vim.fn.col('.') - 1
+
+        if cmp.visible() then
+          cmp.select_next_item(select_opts)
+        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+          fallback()
+        else
+          cmp.complete()
+        end
+      end, {'i', 's'}),
+
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item(select_opts)
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
+    }),
+
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` 
+  -- (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' 
+  -- (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+end
+
+
+
+-- --------------
+-- Manage plugins
+-- --------------
+--
 return {
-{
-    "williamboman/mason.nvim",
+  {
+    "williamboman/mason-lspconfig.nvim",
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason.nvim",
         "neovim/nvim-lspconfig",
+        -- nvim-cmp (not dependencies of mason-lspconfig)
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
     },
     config = config_mason_lsp,
-},
+  },
 }
