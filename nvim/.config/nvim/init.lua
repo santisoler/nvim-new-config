7c30c1d3ea3ddecb3587c29c1385a4305eb0ba97
Santiago Soler <santisoler@fastmail.com>
2024-12-01 15:38:10 -0800

    Add autocommands and custom functions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
 require("config.lazy")
 require("config.base")
 require("config.keymappings")
+require("config.functions")
+require("config.autocmd")
 require("config.lspattach")
 require("config.diagnostics")
