1644a9e383f47816db4e013f2362ebcbddcc575c
Santiago Soler <santisoler@fastmail.com>
2024-12-01 15:38:55 -0800

    Configure catppuccin

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,19 @@
-return { 
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin"
-        end
-    }
+local function config()
+	require("catppuccin").setup({
+		no_italic = true, -- Force no italic
+		native_lsp = {
+			enabled = true,
+		},
+	})
+	-- setup must be done before loading
+	vim.cmd.colorscheme("catppuccin")
+end
+
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = config,
+	},
 }
